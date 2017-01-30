7c1b6e08ef6cf0c7bf2ba46b7d6217822b77b88d
Levi Thatcher <levithatcher@users.noreply.github.com>
2017-01-30 15:39:57 -0700

    Improving update instructions sphinx

--- update_pysphinx.sh
+++ update_pysphinx.sh
@@ -6,14 +6,15 @@
 
 # 2) Install the packages in dev-requirements.txt
 
-# Run this from within the top of the documentation repo
+# Run this from within the root of the healthcareai-py repo
 
-#sphinx-build -b html docs py
+# Build docs
+sphinx-build -b html docs py
 
 cd ../healthcareai-py
 
+# Copy py folder to documentation repo
 cp -rf py ../documentation
 
+# Delete new py folder in the healthcareai-py directory
 rm -rf py
-
-cd ../documentation
