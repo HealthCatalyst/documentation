87184f0c3c1ea4d72a12d7a24c4b78462b80b9fe
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-02-03 17:58:39 -0700

    - more gitignore goodness - retooled jekyll update bash script to clean up it's mess better and - cleaned up lots more recursive detritus generated files - changed straggler hctools to .ai

---
+++ remove_old_blog_files.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Delete old blog (both the generated and final destination)
+rm -rf blog assets splashcss splashimg splashjs splashless splashvendor jekyll/_site
--- update_jekyll.sh
+++ update_jekyll.sh
@@ -1,25 +1,24 @@
+#!/usr/bin/env bash
 # Update jekyll blog
 
-# Delete old copy
-rm -rf blog
+# Delete old blog (both the generated and final destination)
+echo 'Deleting old files'
+./remove_old_blog_files.sh
 
-ls -la
-
+echo 'Building new blog files'
 cd jekyll
-
 jekyll build
 
 # Move into where new blog html files are
 cd _site
 
 # Move blog main page into /blog (so it shows at healthcare.ai/blog instead of at healthcare.ai)
-mv index.html blog
-
-# Change (what will be) top-level blog to newblog for testing
+# TODO fix this hack so it doesn't use relative links #215-6
+mv index.html blog/
 
 # Move everything within _site to healthcare.ai/blog
-cp -rf * ../..
+#echo 'Moving files'
+mv ./* ../../
 
-
-
+echo 'Blog generated.'
 
