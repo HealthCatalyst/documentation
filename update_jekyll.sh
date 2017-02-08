02077e438a475394dcfa69376b55fec5e70880e3
Aylr <taylor@00.local>
2017-02-07 22:18:18 -0700

    * made the dangerous bash script hopefully less dangerous after it wiped out hours of work. * made blog index and blog posts have margins * defuglied the blog index list

--- update_jekyll.sh
+++ update_jekyll.sh
@@ -7,18 +7,24 @@ echo 'Deleting old files'
 
 echo 'Building new blog files'
 cd jekyll
-jekyll build
 
-# Move into where new blog html files are
-cd _site
+if jekyll build; then
+    # only do move operations if the _site dir exists, otherwise you'll wreak havoc
+    if [ -d "_site" ]; then
 
-# Move blog main page into /blog (so it shows at healthcare.ai/blog instead of at healthcare.ai)
-# TODO fix this hack so it doesn't use relative links #215-6
-mv index.html blog/
+        # Move into where new blog html files are
+        cd _site
 
-# Move everything within _site to healthcare.ai/blog
-#echo 'Moving files'
-mv ./* ../../
+        # Move blog main page into /blog (so it shows at healthcare.ai/blog instead of at healthcare.ai)
+        # TODO fix this hack so it doesn't use relative links #215-6
+        mv index.html blog/
 
-echo 'Blog generated.'
+        # Move everything within _site to healthcare.ai/blog
+        #echo 'Moving files'
+        mv ./* ../../
 
+        echo 'Blog generated.'
+    fi
+else
+    echo 'Build failed, aborting'
+fi
