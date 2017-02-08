e7210110ffd0e9de2b677b9b45fffc559f77780a
Aylr <taylor@00.local>
2017-02-07 21:00:32 -0700

    * jekyll include-ified contact, learn more * fixed some outstanding broken refs to static assets * updated readme * updated removal bash for new jekyll-made files * changed learnmore.html to /learn_more * futzed with jekyll config site/url/baseurl vars * made lots of new clean includes * removed some now obsolete includes

--- remove_old_blog_files.sh
+++ remove_old_blog_files.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Delete old blog (both the generated and final destination)
-rm -rf blog assets splashcss splashimg splashjs splashless splashvendor jekyll/_site feed.xml feed.xslt.xml
+rm -rf blog assets splashcss splashimg splashjs splashless splashvendor jekyll/_site feed.xml feed.xslt.xml contact.html learn_more
