ebb94d81162b6f8cbd8a6c7ab8186d2efcaba9ac
Aylr <taylor@00.local>
2017-02-08 11:53:40 -0700

    * major refactor to make jekyll build the main page * moved public assets to jekyll/ folder as the source * exclude public on jekyll config (so it doesn’t recompile bootstrap) * this required … * new bash script to cherry pick the compiled public assets (slightly brittle) to the root * modified bash script that cleans in prep for jekyll build * documented  all .html pages that jekyll parses * rebuilt all generated assets * removed index.md stub

--- remove_old_blog_files.sh
+++ remove_old_blog_files.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Delete old blog (both the generated and final destination)
-rm -rf blog assets jekyll/_site feed.xml feed.xslt.xml contact.html learn_more
+rm -rf blog assets learn_more public index.html jekyll/_site feed.xml feed.xslt.xml contact.html
